a1b6a8819213bd1ceba7e97a5f7e028f5508f640
OmarAliSoliman <omarasmar9955@gmail.com>
2023-09-12 21:33:44 +0300

    menue

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,49 +63,6 @@ $(document).ready(function () {
   }
 
 
-  // shows-item-section-slider
-
-  if ($(".personal-img").length) {
-    $(document).on("change", ".personal-img-file", function () {
-      var uploadFile = $(this);
-      var files = !!this.files ? this.files : [];
-      if (!files.length || !window.FileReader) return; // no file selected, or no FileReader support
-
-      if (/^image/.test(files[0].type)) {
-        // only image file
-        var reader = new FileReader(); // instance of the FileReader
-        reader.readAsDataURL(files[0]); // read the local file
-
-        reader.onloadend = function () {
-          // set image data as background of div
-          //alert(uploadFile.closest(".upimage").find('.imagePreview').length);
-          uploadFile
-            .closest(".personal-img")
-            .find(".per")
-            .css("background-image", "url(" + this.result + ")");
-          $(".personal-img-validation-delete").css("display", "block");
-        };
-      }
-    });
-  }
-
-  if ($(".custom_input_file").length) {
-    $("#inputfile").on("change", function (e) {
-      let file_name = e.target.files[0].name;
-      console.log(this.files[0].mozFullPath);
-      // console.log(file_name)
-      let catditem = $(this)
-        .closest(".moda_images")
-        .find(".card-img:last")
-        .clone();
-      // $(this).closest(".moda_images .images_file").append(catditem);
-      $(catditem).insertBefore(".custom_input_file");
-      $(catditem)
-        .find(".img-parent img")
-        .attr("src", `./assets/images/${file_name}`);
-    });
-  }
-
   $(".custom-navbar #nav-icon1").click(function () {
     $(".side-nav").addClass("side-nav-open");
     $("html").addClass("side-active");
@@ -126,29 +83,30 @@ $(document).ready(function () {
     });
   }
 
-  if ($(".custom-select").length) {
-    $(".custom-select").niceSelect();
-  }
 
   if ($(".animate__animated").length) {
     new WOW().init();
   }
 
-  if($(".custom-navbar").length){
-    $(".custom-navbar .search").click(function () {
-      $(".search-screen").addClass("search-screen-active");
-      $("html").addClass("side-active");
-    })
-  }
-
-  if($(".search-screen").length){
-    $(".search-screen .close").click(function () {
-      $(".search-screen").removeClass("search-screen-active");
-      $("html").removeClass("side-active");
-    })
-  }
-
   $("html").addClass("splash-active");
+
+  if($(".custom-navbar").length){
+    $(".custom-navbar .submenuparent .submenu li a").on('mouseover', function(){
+      var datamenu = $(this).attr('data-menu');
+      $(`#${datamenu}`).addClass('activebigmenu')
+    })
+    $(".custom-navbar .submenuparent .submenu li a").on('mouseleave', function(){
+      var datamenu = $(this).attr('data-menu');
+      $(`#${datamenu}`).removeClass('activebigmenu')
+    })
+
+    $(".top-submenu").on('mouseover', function(){
+      $(this).addClass('activebigmenu')
+    })
+    $(".top-submenu").on('mouseleave', function(){
+      $(this).removeClass('activebigmenu')
+    })
+  }
 
 });
 
