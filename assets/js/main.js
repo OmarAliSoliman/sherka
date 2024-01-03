8c3e941f5e4ba8958d7345664e84b2645b4ab610
OmarAliSoliman <omarasmar9955@gmail.com>
2024-01-04 00:45:38 +0200

    f

--- assets/js/main.js
+++ assets/js/main.js
@@ -144,6 +144,47 @@ $(document).ready(function () {
     })
   }
 
+
+
+  if ($(".teste_cards-slider").length) {
+    $(".teste_cards-slider").slick({
+      slidesToShow: 4,
+      rtl: currentDir == "rtl" ? true : false,
+      arrows: false,
+      dots: false,
+      slidesToScroll: 1,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+          },
+        },
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    })
+  }
+
+
+  AOS.init();
+
 });
 
 
